4172b22d8d6bfc24e7bea18bd1072e3958994215
sjordanoff <sijordanoff@gmail.com>
2018-04-02 17:49:26 +0300

    .

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,26 @@
+#a
+x <- c(4,1,1,4)
+
+#b
+y <- c(1,4)
+
+#c
+x-y
+#subtraction is performed elementwise and if two vectors are of unequal length
+#d 
+s <- c(x,y)
+
+#e
+rep(s, 10)
+length(rep(s, 10))
+
+#f
+rep(s, each=3)
+
+#g
+seq.default(from=7, to=21, by=1)
+7:21
+
+#h
+length(7:21)
 
